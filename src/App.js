f7af2237e34fc7a3a7160aea2a257d755c1d86b9
kristiangali <56776544+kristiangali@users.noreply.github.com>
2019-10-24 21:17:57 -0600

    Simple App.js Update
    Simple App.JS Change and Update to test github.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kristian's React Playground
         </a>
       </header>
     </div>
